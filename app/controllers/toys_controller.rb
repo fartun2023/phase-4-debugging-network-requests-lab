39854a3bd1bb562fc2cf2024c23d0ce4b9be4346
fartun <fartunwario34@gmail.com>
2023-06-24 03:36:15 +0300

    completed assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,19 +7,36 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
-    render json: toy, status: :created
+    toy = Toy.new(toy_params)
+    if toy.save
+      render json: toy, status: :created
+    else
+      render json: { errors: toy.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
   end
+  
+  def like
+    toy = Toy.find(params[:id])
+    toy.likes += 1
+    if toy.save
+      render json: toy, status: :ok
+    else
+      render json: { errors: toy.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
-    toy.destroy
-    head :no_content
+    toy = Toy.find(params[:id])
+    if toy.destroy
+      head :no_content
+    else
+      render json: { errors: toy.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
@@ -27,5 +44,4 @@ class ToysController < ApplicationController
   def toy_params
     params.permit(:name, :image, :likes)
   end
-
 end
